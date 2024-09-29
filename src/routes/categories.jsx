ed4bb6efdd3c1a1df906ea2b73861c9d1bc86a6b
Billy <billydevbusiness@gmail.com>
2024-09-29 13:07:17 +0500

    feat: Categories page

---
+++ src/routes/categories.jsx
@@ -0,0 +1,121 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "../index.css";
+import styles from "./categories.module.css";
+
+import actionImg from "../assets/action.png";
+import dramaImg from "../assets/drama.png";
+import fantasyImg from "../assets/fantasy.png";
+import fictionImg from "../assets/fiction.png";
+import horrorImg from "../assets/horror.png";
+import musicImg from "../assets/music.png";
+import romanceImg from "../assets/romance.png";
+import thrillerImg from "../assets/thriller.png";
+import westernImg from "../assets/western.png";
+
+const categories = [
+  { name: "Action", color: "#ff5108", image: actionImg },
+  { name: "Drama", color: "#d7a4ff", image: dramaImg },
+  { name: "Romance", color: "#148a08", image: romanceImg },
+  { name: "Thriller", color: "#84c2ff", image: thrillerImg },
+  { name: "Western", color: "#902500", image: westernImg },
+  { name: "Horror", color: "#7358ff", image: horrorImg },
+  { name: "Fantasy", color: "#ff4ade", image: fantasyImg },
+  { name: "Music", color: "#e61e32", image: musicImg },
+  { name: "Fiction", color: "#6cd061", image: fictionImg },
+];
+
+const CategoriesPage = () => {
+  const [selected, setSelected] = useState([]);
+  const navigate = useNavigate();
+
+  const toggleCategory = (category) => {
+    setSelected((prev) =>
+      prev.some((c) => c === category)
+        ? prev.filter((c) => c !== category)
+        : [...prev, category]
+    );
+  };
+
+  useEffect(() => {
+    // const user = JSON.parse(localStorage.getItem("user"));
+    // if (user.categories.length > 0) {
+    //   navigate("/dashboard");
+    // }
+  }, []);
+
+  const saveCategories = () => {
+    const user = JSON.parse(localStorage.getItem("user"));
+    user.categories = selected;
+    localStorage.setItem("user", JSON.stringify(user));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log("Categories picking successful");
+    saveCategories();
+    navigate("/dashboard");
+  };
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.leftSection}>
+        <h1 className="appName">Super app</h1>
+        <h2 className={styles.title}>Choose your entertainment category</h2>
+        <div
+          className={`${styles.selectedCategories} ${
+            selected.length > 0 ? styles.visible : styles.hidden
+          }`}
+        >
+          {selected.map((category) => (
+            <span key={category} className={styles.selectedCategory}>
+              {category}{" "}
+              <button onClick={() => toggleCategory(category)}>x</button>
+            </span>
+          ))}
+        </div>
+        {
+          <p
+            className={`${styles.warning} ${
+              selected.length < 3 ? styles.visible : styles.hidden
+            }`}
+          >
+            Minimum 3 category required
+          </p>
+        }
+      </div>
+
+      <div className={styles.rightSection}>
+        <div className={styles.categoryGrid}>
+          {categories.map((category) => (
+            <button
+              key={category.name}
+              className={styles.categoryButton}
+              style={{
+                backgroundColor: category.color,
+                boxShadow: selected.includes(category.name)
+                  ? "0 0 0 4px #148A14" // border hack
+                  : "none",
+              }}
+              onClick={() => toggleCategory(category.name)}
+            >
+              <h3>{category.name}</h3>
+              <img src={category.image} alt={category.name} />
+            </button>
+          ))}
+        </div>
+        <div className={styles.nextButtonContainer}>
+          <button
+            className={styles.nextButton}
+            disabled={selected.length < 3}
+            onClick={handleSubmit}
+          >
+            Next Page
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CategoriesPage;
